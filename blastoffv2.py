f849fb5c80602cbdbcaec974c128e0e54150ffac
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-04-28 10:28:24 -0400

    Update blastoffv2.py

--- blastoffv2.py
+++ blastoffv2.py
@@ -1,11 +1,8 @@
 import time
-
-
 count = 5
-tmin = "T-{}"
 while count > 0:
+        print("T-" + str(count))
         time.sleep(1)
-        print(tmin.format(count))
         count -= 1
 else:
         print("Blast Off!")

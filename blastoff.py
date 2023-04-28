a6a7a65e7ef56775ab54ab5aa012e8081871f85b
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-04-27 23:15:42 -0400

    Getting up to date
    Adding all my python files that I have right now.

---
+++ blastoff.py
@@ -0,0 +1,3 @@
+blastoff = ["T-5", "T-4", "T-3", "T-2", "T-1", "Blast-Off!"]
+for x in blastoff:
+    print (x)
---
+++ blastoffv2.py
@@ -0,0 +1,7 @@
+count = -1
+tmin = "T{}"
+while count > -6:
+        print(tmin.format(count))
+        count -= 1
+else:
+        print("Blast Off!")

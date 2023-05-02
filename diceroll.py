4bdd874ace71002e63d9975481845a6cc3285d00
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-05-02 07:53:11 -0400

    Update diceroll.py

--- diceroll.py
+++ diceroll.py
@@ -1,12 +1,21 @@
-print()
-print("How many dice?")
-num = int(input())
-print()
-print("How many sides?")
-randomRange = int(input())
-print()
+import random
 
-while num > 0:
-    ranNum = random.randrange(1, randomRange)
-    print("nat"+str(ranNum))
-    num -= 1
+
+rolling = "y"
+while rolling == "y":
+    print()
+    print("How many dice?")
+    num = int(input())
+    print()
+    print("How many sides?")
+    randomRange = int(input())
+    print()
+
+    while num > 0:
+        ranNum = random.randrange(1, randomRange)
+        print("nat"+str(ranNum))
+        num -= 1
+
+    rolling = input("Roll again??")
+else:
+    print("Come again!")

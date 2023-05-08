905c21916908610e8b6e281504285efd3ce4b689
stormybeat <128640313+stormybeat@users.noreply.github.com>
2023-05-08 17:48:51 -0400

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,15 +1,21 @@
-# This example requires the 'message_content' intent.
 import discord
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
 client.run('MTA5NjE2MTQxODU4MTExNTA0Mg.GzZd_S.2MuDgBnOlqLhBuPIQ59pqbss8Lhe4i0m0jq1zI')
